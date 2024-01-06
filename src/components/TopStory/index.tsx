dde3d33e76243dd83de87fbedb149eb038734d9a
samane <samane.teymurifar@gmail.com>
2024-01-06 14:22:52 +0330

    change style

--- src/components/TopStory/index.tsx
+++ src/components/TopStory/index.tsx
@@ -6,9 +6,8 @@ import { isArray } from "@src/utiles/helper/isArray";
 async function TopStories() {
     const response = await getData();
     const TopStoriesData = (response?.result?.trending)?.products.slice(0,8)
- 
     return (
-    <section className="w-full bg-common-white py-4 flex justify-center my-6 gap-[50px]">
+    <section className="w-full bg-common-white py-6 flex justify-center my-6 gap-[50px]">
       {isArray(TopStoriesData).map((item:HomeTrendingDataType) => {
         return (
           <StoryItem key={item.id} item={item} TopStoriesData={TopStoriesData}/>
