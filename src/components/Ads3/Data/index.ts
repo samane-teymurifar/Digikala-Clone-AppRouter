814e042f7356f3ee540a5c1f9d8f7a9ce8de3361
samane <samane.teymurifar@gmail.com>
2024-01-16 18:45:05 +0330

    add some data for banner

---
+++ src/components/Ads3/Data/index.ts
@@ -0,0 +1,18 @@
+import { AdsDataType } from "../Type";
+
+
+  const AdsData: AdsDataType[] = [
+    {
+      id: "1",
+      src: "https://dkstatics-public.digikala.com/digikala-adservice-banners/2868645aeeea88e39faef0ee16ce1e47e15ebada_1705238744.jpg?x-oss-process=image/quality,q_95/format,webp",
+        title: "پارتنرشیپ-چرم مشهد",
+        alt: "پارتنرشیپ-چرم مشهد"
+    },
+    {
+        id: "2",
+        src: "https://dkstatics-public.digikala.com/digikala-adservice-banners/bf92fba2b245d680aad7ab89659e4a1293c559f4_1704974447.jpg?x-oss-process=image/quality,q_95/format,webp",
+        alt: "پارتنرشیپ-فیروز"
+    },
+
+  ];
+  export default AdsData;
---
+++ src/components/BannerAd/Data/index.ts
@@ -0,0 +1,13 @@
+import { AdsDataType } from "../Type";
+
+
+  const BannerAdData: AdsDataType[] = [
+    {
+      id: "1",
+      src: "https://dkstatics-public.digikala.com/digikala-adservice-banners/4e38b7401f8d5b77eefbd36c98f9d10779177663_1704812847.jpg?x-oss-process=image/quality,q_95/format,webp",
+        title: "پارتنرشیپ-بومرنگ",
+        alt: "پارتنرشیپ-بومرنگ"
+    }
+
+  ];
+  export default BannerAdData;
