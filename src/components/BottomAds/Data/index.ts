a3b24b3d5eedb5dd255048794dd3dc9a244e781f
samane <samane.teymurifar@gmail.com>
2024-01-16 18:14:21 +0330

    add some data

---
+++ src/components/BottomAds/Data/index.ts
@@ -0,0 +1,19 @@
+import { AdsDataType } from "../Type";
+
+
+  const AdsData: AdsDataType[] = [
+    {
+      id: "1",
+      src: "https://dkstatics-public.digikala.com/digikala-adservice-banners/792b26d05b1af23cc09a37969d8739caae938b01_1704715052.gif?x-oss-process=image?x-oss-process=image/format,webp",
+        title: "پارتنرشیپ-سیف",
+        alt: "پارتنرشیپ-سیف"
+    },
+    {
+        id: "2",
+        src: "https://dkstatics-public.digikala.com/digikala-adservice-banners/2a36a2a56a08554e6b13e70edbfa2d349cdefc8f_1705140698.jpg?x-oss-process=image/quality,q_95/format,webp",
+        title: "پارتنرشیپ-اکتیو",
+        alt: "پارتنرشیپ-اکتیو"
+    },
+
+  ];
+  export default AdsData;
