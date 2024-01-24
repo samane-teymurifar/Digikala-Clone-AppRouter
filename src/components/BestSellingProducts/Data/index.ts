40bed57f38458321327dcb550215b0b5b1d64cc7
samane <samane.teymurifar@gmail.com>
2024-01-24 23:57:29 +0330

    add some data

---
+++ src/components/BestSellingProducts/Data/index.ts
@@ -0,0 +1,30 @@
+import {BazarcheCardType} from "../Type/index";
+
+  const BestSelingProduct: BazarcheCardType[] = [
+    {
+      id: 1,
+      src: "https://dkstatics-public.digikala.com/digikala-products/6ea8d1688bd9dda01dc7f5716bb7c84e9d9c3a0d_1652694146.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/format,webp/quality,q_80",
+ 
+    },
+    {
+        id: 2,
+        src: "https://dkstatics-public.digikala.com/digikala-products/f55ceeffca5e2bdcc904dd939abc37e36cb0ad82_1657598550.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+   
+      },
+      {
+        id: 3,
+        src: "https://dkstatics-public.digikala.com/digikala-adservice-banners/a35f5dc132f1c28e1652a8e9e41e92375226e60f_1704715860.jpg?x-oss-process=image/quality,q_95/format,webp",
+   
+      },{
+        id: 4,
+        src: "https://dkstatics-public.digikala.com/digikala-products/9437312ce7c2cdf0d95633ff1688e98414b94c03_1652782557.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/format,webp/quality,q_80",
+   
+      },
+      {
+        id: 5,
+        src: "https://dkstatics-public.digikala.com/digikala-products/10cc5a746e874fb3487a08e94a0244e751576eb2_1639203907.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/format,webp/quality,q_80"
+      },{
+        
+      }
+  ];
+  export default BestSelingProduct;
