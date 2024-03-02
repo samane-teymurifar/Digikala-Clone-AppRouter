18f1583f3bf85b52f372135c4ad65401dcd3aad0
samane <samane.teymurifar@gmail.com>
2024-03-02 13:47:34 +0330

    [FIX]:megaMenue style

--- src/layouts/header/navBar/mega-menu/index.tsx
+++ src/layouts/header/navBar/mega-menu/index.tsx
@@ -9,7 +9,7 @@ async function MegaMenu({clasificationList}: any) {
   const clasificationListSecond = someJson.result.widgets;
 
   return (
-    <>
+    <div>
       <MegaMenuButton />
       <div
         className="bg-common-white h-auto z-20 border absolute mt-[3000px] w-fit"
@@ -17,8 +17,8 @@ async function MegaMenu({clasificationList}: any) {
         <div>
           {isArray(clasificationListSecond)
             .slice(0, 12)
-            .map(cluster => (
-              <div className="flex">
+            .map((cluster, index) => (
+              <nav className="flex w-[700px]">
                 <div className="flex items-center w-[25%]">
                   <Link
                     href={''}
@@ -26,16 +26,16 @@ async function MegaMenu({clasificationList}: any) {
                     {cluster.data.title}
                   </Link>
                 </div>
-                <div className="border w-[75%]">
+                <div className={`${index === 1 && 'w-75' }border categoryItem`}>
                   {cluster?.data?.child?.map((item: any) => {
                     return <div className="text-gray-90">{item?.title_fa}</div>;
                   })}
                 </div>
-              </div>
+              </nav>
             ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
